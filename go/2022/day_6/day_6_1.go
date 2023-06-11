2e513c7892e6c278fa2483dd9056c43f605a7af3
William Juang <juangw@umich.edu>
2023-06-11 11:38:46 -0500

    finish day 6 problem 1

--- go/2022/day_6/day_6_1.go
+++ go/2022/day_6/day_6_1.go
@@ -6,6 +6,20 @@ import (
 	"strings"
 )
 
+func containsDuplicate(strings []string) bool {
+	disct := make(map[string]bool, len(strings))
+
+	for _, str := range strings {
+		if disct[str] {
+			return true
+		} else {
+			disct[str] = true
+		}
+	}
+	return false
+
+}
+
 func main() {
 	packets, err := ioutil.ReadFile("day_6.txt")
 	if err != nil {
@@ -13,11 +27,29 @@ func main() {
 	}
 
 	var packetLines = strings.Split(string(packets), "\n")
-	var uniqueChars = make([]string, 4, 4)
+	var uniqueChars = make([]string, 0, 4)
 	for i := 0; i < len(packetLines); i++ {
-		fmt.Println(packetLines[i])
 		for j := 0; j < len(packetLines[i]); j++ {
-			uniqueChars = append(uniqueChars, string(packetLines[i][j]))
+			var char = string(packetLines[i][j])
+			var foundDuplicate = false
+
+			if len(uniqueChars) < 4 {
+				uniqueChars = append(uniqueChars, char)
+			}
+
+			if len(uniqueChars) == 4 {
+				// iterate through uniqueChars for duplicates
+				if containsDuplicate(uniqueChars) {
+					foundDuplicate = true
+				}
+			}
+
+			if foundDuplicate {
+				uniqueChars = uniqueChars[1:]
+			} else if len(uniqueChars) == 4 {
+				fmt.Println(j + 1)
+				break
+			}
 		}
 	}
 	fmt.Println(uniqueChars)
